51e2ee8fc0b182f20a66e77149751c98b4387b19
Luis  Montoya <luismec90@gmail.com>
2020-08-25 15:45:55 -0500

    Updates

---
+++ precache-manifest.f9db6a8b5efc7a1a2c7a2b7731438c64.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ca4806899607c0769c619055d6f6824",
+    "url": "/test3/index.html"
+  },
+  {
+    "revision": "31ee833df0c76e6badee",
+    "url": "/test3/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9f8ac2db648176a72412",
+    "url": "/test3/static/js/2.261adc95.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test3/static/js/2.261adc95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31ee833df0c76e6badee",
+    "url": "/test3/static/js/main.a55b62c2.chunk.js"
+  },
+  {
+    "revision": "fa748e0b0ba1e6ef4344",
+    "url": "/test3/static/js/runtime-main.08fa0b29.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test3/static/media/logo.5d5d9eef.svg"
+  }
+]);
